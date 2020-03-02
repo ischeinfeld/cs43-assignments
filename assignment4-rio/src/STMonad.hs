8d049fc6356c9334e2bfb91153c6b978973da630
Adithya Ganesh <acganesh@stanford.edu>
2020-03-01 17:25:33 -0800

    add ST problem

---
+++ assignment4-rio/src/STMonad.hs
@@ -0,0 +1,26 @@
+import Control.Monad.ST
+import Data.STRef
+import Control.Monad
+
+sumST :: Num a => [a] -> a
+sumST xs = runST $ do
+  n <- newSTRef 0
+
+  forM_ xs $ undefined
+
+  readSTRef n
+
+foldlST :: (a -> b -> a) -> a -> [b] -> a
+foldlST f acc xs = undefined
+
+-- Might be useful -- from Control.Monad.Loops
+whileM_ :: (Monad m) => m Bool -> m a -> m ()
+whileM_ p f = go
+    where go = do x <- p
+                  if x
+                     then f >> go
+                     else return ()
+
+collatz :: Integer -> Integer
+collatz n | n > 0 = undefined
+          | otherwise = error "n must be positive"
